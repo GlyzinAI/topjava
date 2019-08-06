945ab8b33b6c344c2e0a501c3e56080a8572b549
GlyzinAI <glizin.art@gmail.com>
2019-08-07 01:18:23 +0300

    HW#8

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,65 @@
+$(function () {
+    makeEditable({
+            ajaxUrl: "ajax/meals/",
+            datatableApi: $("#datatable").DataTable({
+                "paging": false,
+                "info": true,
+                "columns": [
+                    {
+                        "data": "dateTime"
+                    },
+                    {
+                        "data": "description"
+                    },
+                    {
+                        "data": "calories"
+                    },
+                    {
+                        "defaultContent": "Edit",
+                        "orderable": false
+                    },
+                    {
+                        "defaultContent": "Delete",
+                        "orderable": false
+                    }
+                ],
+                "order": [
+                    [
+                        0,
+                        "desk"
+                    ]
+                ]
+            })
+        }
+    );
+});
+
+function filter() {
+    $.get(context.ajaxUrl + "filter", $('#dateForm').serialize())
+        .done(function (data) {
+            context.datatableApi.clear().rows.add(data).draw()
+        });
+}
+
+function resetForm() {
+    $('#dateForm').find(':input').val("");
+    updateTable();
+}
+
+function saveMeal() {
+    var formM = $("#dateForm");
+    $.ajax({
+        type: "POST",
+        url: context.ajaxUrl,
+        data: form.serialize()
+    }).done(function () {
+        $("#editRow").modal("hide");
+        if (formM.find('#startDate').val() || formM.find('#startTime').val() || formM.find('#endDate').val() || formM.find('#endTime').val()) {
+            filter();
+        }
+        else {
+            updateTable();
+        }
+        successNoty("Saved");
+    });
+}
