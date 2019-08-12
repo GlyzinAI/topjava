79bcbf6bb58a3fb7d6e07f23e2f8795f5e2d778b
GlyzinAI <glizin.art@gmail.com>
2019-08-13 00:25:32 +0300

    HW#9 without optional

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -3,9 +3,12 @@ package ru.javawebinar.topjava.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -55,6 +58,12 @@ public class MealService {
     public Meal create(Meal meal, int userId) {
         Assert.notNull(meal, "meal must not be null");
         return repository.save(meal, userId);
+    }
+
+    @Transactional
+    public void update(MealTo mealTo, int userId) {
+        Meal meal = get(mealTo.getId(), userId);
+        repository.save(MealsUtil.updateFromTo(meal, mealTo), userId);
     }
 
     public Meal getWithUser(int id, int userId) {
--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,14 +1,23 @@
 package ru.javawebinar.topjava.to;
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class MealTo extends BaseTo {
 
+    @NotNull
     private LocalDateTime dateTime;
 
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String description;
 
+    @Range(min = 10, max = 5000)
     private int calories;
 
     private boolean excess;
@@ -21,6 +30,22 @@ public class MealTo extends BaseTo {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.excess = excess;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public void setExcess(boolean excess) {
         this.excess = excess;
     }
 
